0bb2453dc0fd3e5e5cfdde5e9398a848e442a2a8
Arswarog <arswarog@yandex.ru>
2018-11-08 00:57:00 +0300

    Stickers

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,34 @@ class App extends React.Component {
         return (
             <div>
                 <Calculator/>
+                <img src="https://www.picpng.com/images/large/sticky-notes-download-png-32163" style={{
+                    position : 'absolute',
+                    top      : '381px',
+                    left     : '808px',
+                    zIndex   : 5,
+                    transform: 'rotate(12deg)',
+                    filter   : 'hue-rotate(120deg)',
+                }}/>
+                <img src="https://www.picpng.com/images/large/sticky-notes-download-png-32163" style={{
+                    position : 'absolute',
+                    top      : '481px',
+                    left     : '808px',
+                    zIndex   : 5,
+                    transform: 'rotate(-3deg)',
+                }}/>
+                <img src="https://www.picpng.com/images/large/sticky-notes-download-png-32163" style={{
+                    position : 'absolute',
+                    top      : '598px',
+                    left     : '808px',
+                    zIndex   : 5,
+                    transform: 'rotate(6deg)',
+                    filter   : 'hue-rotate(31deg)',
+                }}/>
+                <img src="https://cdn.pixabay.com/photo/2017/01/12/17/11/post-it-1975179_960_720.png" style={{
+                    position: 'absolute',
+                    zIndex  : 5,
+                    display : 'none',
+                }}/>
                 {/*<img src={original} width="100%"/>*/}
                 <div className="paper">
                     <div className="content">
