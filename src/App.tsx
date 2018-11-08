f256b31d081bf7118d83964f721f441e4f913218
Arswarog <arswarog@yandex.ru>
2018-11-08 08:32:54 +0300

    Stickers

--- src/App.tsx
+++ src/App.tsx
@@ -8,66 +8,130 @@ import { StackView } from './components/StackView';
 
 class App extends React.Component {
     public render() {
+        const stickers: Array<{
+            top: number,
+            left: number,
+            text: string,
+            active?: boolean,
+            rotate: number,
+            link?: string,
+            hueRotate: number,
+        }> = [
+            {
+                top      : 598,
+                left     : 808,
+                rotate   : -3,
+                hueRotate: 31,
+                text     : 'Main',
+            },
+            {
+                top      : 598,
+                left     : 808,
+                rotate   : 12,
+                hueRotate: 31,
+                text     : 'Main 2',
+            },
+            {
+                top      : 598,
+                left     : 808,
+                rotate   : -7,
+                hueRotate: 31,
+                text     : 'Main 3',
+            },
+            {
+                top      : 598,
+                left     : 808,
+                rotate   : -3,
+                hueRotate: 31,
+                text     : 'Main 3',
+            },
+            {
+                top      : 598,
+                left     : 808,
+                rotate   : 16,
+                hueRotate: 31,
+                text     : 'Main 3',
+            },
+        ];
+
         return (
             <div>
                 <Calculator/>
-                <img src="https://www.picpng.com/images/large/sticky-notes-download-png-32163" style={{
-                    position : 'absolute',
-                    top      : '381px',
-                    left     : '808px',
-                    zIndex   : 5,
-                    transform: 'rotate(12deg)',
-                    filter   : 'hue-rotate(120deg)',
-                }}/>
-                <img src="https://www.picpng.com/images/large/sticky-notes-download-png-32163" style={{
-                    position : 'absolute',
-                    top      : '481px',
-                    left     : '808px',
-                    zIndex   : 5,
-                    transform: 'rotate(-3deg)',
-                }}/>
-                <img src="https://www.picpng.com/images/large/sticky-notes-download-png-32163" style={{
-                    position : 'absolute',
-                    top      : '598px',
-                    left     : '808px',
-                    zIndex   : 5,
-                    transform: 'rotate(6deg)',
-                    filter   : 'hue-rotate(31deg)',
-                }}/>
-                <img src="https://cdn.pixabay.com/photo/2017/01/12/17/11/post-it-1975179_960_720.png" style={{
-                    position: 'absolute',
-                    zIndex  : 5,
-                    display : 'none',
-                }}/>
                 {/*<img src={original} width="100%"/>*/}
                 <div className="paper">
                     <div className="content">
                         asdasdasdasdasasdaaasssssssssssssssssssssd<br/>
-                    ______________________________________________<br/>
+                        ______________________________________________<br/>
                         asdasdasdasdasasdaaasssssssssssssssssssssd<br/>
-                    ______________________________________________<br/>
+                        ______________________________________________<br/>
                         asdasdasdasdasasdaaasssssssssssssssssssssd<br/>
-                    ______________________________________________<br/>
+                        ______________________________________________<br/>
                         asdasdasdasdasasdaaasssssssssssssssssssssd<br/>
-                    ______________________________________________<br/>
+                        ______________________________________________<br/>
                         asdasdasdasdasasdaaasssssssssssssssssssssd<br/>
-                    ______________________________________________<br/>
+                        ______________________________________________<br/>
                         asdasdasdasdasasdaaasssssssssssssssssssssd<br/>
-                    ______________________________________________<br/>
+                        ______________________________________________<br/>
                         asdasdasdasdasasdaaasssssssssssssssssssssd<br/>
-                    ______________________________________________<br/>
+                        ______________________________________________<br/>
                         asdasdasdasdasasdaaasssssssssssssssssssssd<br/>
-                    ______________________________________________<br/>
+                        ______________________________________________<br/>
                         asdasdasdasdasasdaaasssssssssssssssssssssd<br/>
-                    ______________________________________________<br/>
+                        ______________________________________________<br/>
                         asdasdasdasdasasdaaasssssssssssssssssssssd<br/>
-                    ______________________________________________<br/>
+                        ______________________________________________<br/>
                         asdasdasdasdasasdaaasssssssssssssssssssssd<br/>
-                    ______________________________________________<br/>
+                        ______________________________________________<br/>
                         <HistoryView history={[]}/>
                         <StackView/>
                         asdasdasdasdasasdaaasssssssssssssssssssssd<br/>
-                    ______________________________________________<br/>
+                        ______________________________________________<br/>
+                    </div>
+                    <div className="stickers">
+                        {stickers.map(
+                            (sticker, index) =>
+                                <div className="sticker"
+                                     key={index}>
+                                    <div style={{
+                                        backgroundImage: 'url(https://www.picpng.com/images/large/sticky-notes-download-png-32163)',
+                                        transform      : `rotate(${sticker.rotate}deg)`,
+                                        filter         : `hue-rotate(${sticker.hueRotate}deg)`,
+                                    }}>
+                                        {/*<img src=style={{*/}
+                                        {/*position : 'absolute',*/}
+                                        {/*top      : '381px',*/}
+                                        {/*left     : '808px',*/}
+                                        {/*zIndex   : 5,*/}
+                                        {/*}}/>*/}
+                                        {/*<img src="https://www.picpng.com/images/large/sticky-notes-download-png-32163"*/}
+                                        {/*style={{*/}
+                                        {/*position: 'absolute',*/}
+                                        {/*//top      : '481px',*/}
+                                        {/*//left     : '808px',*/}
+                                        {/*//zIndex   : 5,*/}
+                                        {/*//transform: 'rotate(-3deg)',*/}
+                                        {/*}}/>*/}
+                                        <a>
+                                        {sticker.text}
+                                        </a>
+                                    </div>
+                                </div>,
+                        )}
+                        {/*<img src="https://www.picpng.com/images/large/sticky-notes-download-png-32163" style={{*/}
+                        {/*//position : 'absolute',*/}
+                        {/*top      : '481px',*/}
+                        {/*left     : '808px',*/}
+                        {/*zIndex   : 5,*/}
+                        {/*transform: 'rotate(-3deg)',*/}
+                        {/*}}/>*/}
+                        {/*<img src="https://www.picpng.com/images/large/sticky-notes-download-png-32163" style={{*/}
+                        {/*//position : 'absolute',*/}
+                        {/*top      : '598px',*/}
+                        {/*left     : '808px',*/}
+                        {/*zIndex   : 5,*/}
+                        {/*transform: 'rotate(6deg)',*/}
+                        {/*filter   : 'hue-rotate(31deg)',*/}
+                        {/*}}/>*/}
                     </div>
                 </div>
             </div>
