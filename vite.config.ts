1a593de36d931ddd0e4ca7a18d4ff60df09b94e0
Arswarog <arswarog@yandex.ru>
2025-09-19 17:31:27 +0300

    ci: настройка публикации

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '',
     plugins: [react()],
     server: {
         host: '127.0.0.1',
