8112720d0f51381c9ca4ed6b2e3281b4baa63026
Arswarog <arswarog@yandex.ru>
2025-09-21 16:25:16 +0300

    refactor(app): переход на именованные инпуты в app

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import './index.css';
 
-import App from './app/App.tsx';
+import { App } from './app';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
